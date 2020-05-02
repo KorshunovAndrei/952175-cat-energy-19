965ed45a00e722ef4f87264f0a081a4b306c73b5
KorshunovAndrei <a.kopwyhob@gmail.com>
2020-05-02 15:30:18 +0400

    Задание адаптивные декоративные элементы

---
+++ source/js/script.js
@@ -0,0 +1,25 @@
+"use strict";
+
+let nav = document.querySelector(".page-header__nav");
+let navToggle = document.querySelector(".page-header__toggle");
+
+navToggle.classList.remove("page-header__toggle--nojs");
+nav.classList.remove("page-header__nav--nojs");
+
+navToggle.addEventListener("click", function(){
+  if (navToggle.classList.contains("page-header__toggle--on")) {
+    navToggle.classList.remove("page-header__toggle--on");
+    navToggle.classList.add("page-header__toggle--off");
+  } 
+  else {
+    navToggle.classList.add("page-header__toggle--on");
+    navToggle.classList.remove("page-header__toggle--off");
+  }
+
+  if (nav.classList.contains("page-header__nav--opened")) {
+    nav.classList.remove("page-header__nav--opened");
+  } 
+  else {
+    nav.classList.add("page-header__nav--opened");
+  }
+});
